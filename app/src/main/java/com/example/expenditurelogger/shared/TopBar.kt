f7076f04049b50fbb69f34b1280da536e1691a54
JDingo <45366094+JDingo@users.noreply.github.com>
2024-01-25 15:46:24 +0200

    Add conditional rendering for top bar action elements

--- app/src/main/java/com/example/expenditurelogger/shared/TopBar.kt
+++ app/src/main/java/com/example/expenditurelogger/shared/TopBar.kt
@@ -1,25 +1,23 @@
-import androidx.compose.foundation.layout.Arrangement
-import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
+package com.example.expenditurelogger.shared
+
 import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Settings
 import androidx.compose.material3.CenterAlignedTopAppBar
 import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.Icon
 import androidx.compose.material3.IconButton
-import androidx.compose.material3.Scaffold
-import androidx.compose.runtime.Composable
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.material3.TopAppBarDefaults
-import androidx.compose.material3.TopAppBar
-import androidx.compose.material3.rememberTopAppBarState
-import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
+import androidx.compose.runtime.Composable
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
-fun AppTopBar() {
+fun AppTopBar(
+    onSettingClick: (() -> Unit)? = null,
+    onBackNavigationClick: (() -> Unit)? = null,
+) {
     CenterAlignedTopAppBar(
         colors = TopAppBarDefaults.topAppBarColors(
             containerColor = MaterialTheme.colorScheme.primaryContainer,
@@ -29,9 +27,18 @@ fun AppTopBar() {
             Text("Expenditure Logger",
                 maxLines = 1)
         },
+        navigationIcon = {
+            if (onBackNavigationClick != null) {
+                IconButton(onClick = { onBackNavigationClick() }) {
+                    Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "Navigation to Back")
+                }
+            }
+        },
         actions = {
-            IconButton(onClick = { /*TODO*/ }) {
-                Icon(imageVector = Icons.Filled.Settings, contentDescription = "Settings")
+            if (onSettingClick != null) {
+                IconButton(onClick = { /*TODO*/ }) {
+                    Icon(imageVector = Icons.Filled.Settings, contentDescription = "Settings")
+                }
             }
         }
     )
