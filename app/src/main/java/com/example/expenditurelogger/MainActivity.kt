bd8479328baeab5991032ceb35da6d02075e174e
JDingo <45366094+JDingo@users.noreply.github.com>
2024-03-24 13:41:49 +0200

    Add file handling in main / improve navController

--- app/src/main/java/com/example/expenditurelogger/MainActivity.kt
+++ app/src/main/java/com/example/expenditurelogger/MainActivity.kt
@@ -8,17 +8,22 @@ import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.navigation.compose.NavHost
 import androidx.navigation.compose.composable
 import androidx.navigation.compose.rememberNavController
 import com.example.expenditurelogger.camera.CameraActivity
 import com.example.expenditurelogger.home.Home
+import com.example.expenditurelogger.settings.ListOfMerchantsEdit
+import com.example.expenditurelogger.settings.SettingsActivity
 import com.example.expenditurelogger.ui.theme.ExpenditureLoggerTheme
+import com.example.expenditurelogger.utils.FileHandler
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
         setContent {
             ExpenditureLoggerTheme {
                 // A surface container using the 'background' color from the theme
@@ -26,21 +31,56 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    MainApp()
+                    MainApp(false)
                 }
             }
         }
     }
 }
 
-@Preview(showBackground = true)
 @Composable
-fun MainApp() {
+fun MainApp(
+    preview: Boolean
+) {
     val navController = rememberNavController()
 
-    NavHost(navController = navController, startDestination = "home") {
-        composable("home") {  Home(onNavigateToCamera = { navController.navigate("camera") }) }
-        composable("camera") { CameraActivity(onBackNavigationClick = { navController.navigate("home")}) }
+    if (!preview) {
+        FileHandler.init(LocalContext.current)
     }
 
+    NavHost(navController = navController, startDestination = "home") {
+        composable("home") {
+            Home(
+                onNavigateToCamera = { navController.navigate("camera") },
+                onNavigateToSettings = { navController.navigate("settings") }
+            )
+        }
+
+        composable("settings") {
+            SettingsActivity(
+                onBackNavigationClick = { navController.popBackStack() },
+                onDataSendNavigation = { /* TODO */ },
+                onMerchantEditNavigation = { navController.navigate("merchantListEdit") }
+            )
+        }
+
+        composable("merchantListEdit") {
+            ListOfMerchantsEdit(
+                onBackNavigationClick = { navController.popBackStack() }
+            )
+        }
+
+        composable("camera") {
+            CameraActivity(
+                onBackNavigationClick = { navController.popBackStack() }
+            )
+        }
+    }
+
+}
+
+@Composable
+@Preview
+fun MainAppPreview() {
+    MainApp(true)
 }
